b5b0e678c6e5cca972663f4fa62e44998298dbf7
litsazib <roktimsazib92@gmail.com>
2023-03-19 11:31:17 +0600

    modify

--- components/Layouts/about/MilestoneContent.js
+++ components/Layouts/about/MilestoneContent.js
@@ -1,136 +1,103 @@
-import PageAbout from '../../../styles/About.module.scss';
-import BeatLoader from 'react-spinners/BeatLoader';
-import Head from 'next/head';
-import {linkBreak} from '../../../utils/common.js';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import { FiUsers } from 'react-icons/fi';
-import { FaHandshake } from 'react-icons/fa';
-import { TbStairsUp } from 'react-icons/tb';
-import { RiTeamFill } from 'react-icons/ri';
+import PageAbout from "../../../styles/About.module.scss";
+import BeatLoader from "react-spinners/BeatLoader";
+import Head from "next/head";
+import { linkBreak } from "../../../utils/common.js";
+import Card from "react-bootstrap/Card";
+import { FiUsers } from "react-icons/fi";
 
-const PageMilestone = ({ sendcompanyData }) => {
-	// const bgImg={
-	// 	backgroundImage: `url(${sendcompanyData?.module_image})`,
-	// };
-	const contectLoop = sendcompanyData?.content_item.map((content, idx) => {
-		return (
-			<div className="row g-4" key={idx}>
-				<div className="col-lg-8 col-md-6">
-					<h2 className="font-calibri fw-bold p-2 font-poppins ">{content.item_name}</h2>
-					<p className="justify p-2">{linkBreak(content.item_long_desc)}</p>
-				</div>
+const PageMilestone = ({ sendcompanyData, ValusData }) => {
+  console.log(ValusData[0]?.module_name);
 
-				<div className="col-lg-4 col-md-6">
-					<img src={content.item_image} alt={content.item_name} className="img-fluid img-size rounded-5" />
-				</div>
-			</div>
-		);
-	});
-	return (
-		<>
-		<Head>
-			<title>Company Review | IFAD Multi-product</title>
-		</Head>
-			<section id='company-review' className={PageAbout.milestone}>
-				<img src={sendcompanyData?.module_image} alt={sendcompanyData?.module_name} className={PageAbout.bgimg} />
-				<div className={PageAbout.image}>
-					{/* <h1 className=" display-1 fw-bold text-center align-item-center">
-						{sendcompanyData?.module_name ? sendcompanyData?.module_name : <BeatLoader color="#FA3" />}
-					</h1> */}
-				</div>
-			</section>
-			
-			<section id="milestone" className="py-5">
-				<div className="container">
-					{contectLoop ? contectLoop : <p className="text-center">loading...</p>}
-				</div>
-			</section>
+  const contectLoop = sendcompanyData?.content_item.map((content, idx) => {
+    return (
+      <div className="row g-4" key={idx}>
+        <div className="col-lg-8 col-md-6">
+          <h2 className="font-calibri fw-bold p-2 font-poppins ">
+            {content.item_name}
+          </h2>
+          <p className="justify p-2">{linkBreak(content.item_long_desc)}</p>
+        </div>
 
-			{/* values section start */}
+        <div className="col-lg-4 col-md-6">
+          <img
+            src={content.item_image}
+            alt={content.item_name}
+            className="img-fluid img-size rounded-5"
+          />
+        </div>
+      </div>
+    );
+  });
 
-			<section id="values" className='values'>
-				<div className="container">
-					<h1 className='text-center text-white value-align'>Values</h1>
-				</div>
-			</section>
-			<section id="values-content" className=''>
-				<div className="container">
-					<div className="row">
-						<div className='col-lg-3 text-center'>
-							<div className="space-content">
-								<Card className="value-content-bg rounded-4">
-									{/* <Card.Img variant="top" src="holder.js/100px180" /> */}
-									<div>
-										<FiUsers className="mx-4 mt-5 text-center" size={"50px"} color={"white"}/>
-									</div>
-									<Card.Body>
-										<Card.Title className="font-poppins text-white mt-2"><h4 className="fw-700">CUSTOMER FOCUS</h4></Card.Title>
-										<Card.Text className="card-align text-white text-center pb-3 pt-2 ">
-										We recognize that our customers are the reason for our success in business, and we are committed to listen and respond positively to their needs.
-										</Card.Text>
-									</Card.Body>
-								</Card>
-							</div>
-						</div>
+  const valusCard = ValusData[0]?.content_item.map((valus, idx) => {
+    return (
+      <div className="col-lg-3 text-center">
+        <div className="space-content">
+          <Card className="value-content-bg rounded-4">
+            <div className="text-center">
+              <img
+                src={valus.item_image}
+                className="mx-4 mt-5 img-icon-height text-center"
+                alt={valus.item_name}
+              />
+            </div>
+            <Card.Body>
+              <Card.Title className="font-poppins text-white mt-2">
+                <h4 className="fw-700">{valus.item_name}</h4>
+              </Card.Title>
+              <Card.Text className="card-align text-white text-center pb-3 pt-2 ">
+                {valus.item_long_desc}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
+      </div>
+    );
+  });
 
-						<div className='col-lg-3 text-center'>
-						<div className="space-content">
-							<Card className="value-content-bg rounded-4">
-								{/* <Card.Img variant="top" src="holder.js/100px180" /> */}
-								<div>
-									<FaHandshake className="mx-4 mt-5 text-center" size={"50px"} color={"white"}/>
-								</div>
-								<Card.Body>
-									<Card.Title className="font-poppins text-white mt-2"><h4 className="fw-700">RESPONSIBILITIES</h4></Card.Title>
-									<Card.Text className="card-align text-white text-center pb-3 pt-2 ">
-									We recognize that our customers are the reason for our success in business, and we are committed to listen and respond positively to their needs.
-									</Card.Text>
-								</Card.Body>
-							</Card>
-						</div>
-					</div>
+  return (
+    <>
+      <Head>
+        <title>Company Review | IFAD Multi-product</title>
+      </Head>
+      <section id="company-review" className={PageAbout.milestone}>
+        <img
+          src={sendcompanyData?.module_image}
+          alt={sendcompanyData?.module_name}
+          className={PageAbout.bgimg}
+        />
+        <div className={PageAbout.image}></div>
+      </section>
 
-					<div className='col-lg-3 text-center'>
-						<div className="space-content">
-							<Card className="value-content-bg rounded-4">
-								{/* <Card.Img variant="top" src="holder.js/100px180" /> */}
-								<div>
-									<TbStairsUp className="mx-4 mt-5 text-center" size={"50px"} color={"white"}/>
-								</div>
-								<Card.Body>
-									<Card.Title className="font-poppins text-white mt-2"><h4 className="fw-700">ASPIRATION</h4></Card.Title>
-									<Card.Text className="card-align text-white text-center pb-3 pt-2 ">
-									We recognize that our customers are the reason for our success in business, and we are committed to listen and respond positively to their needs.
-									</Card.Text>
-								</Card.Body>
-							</Card>
-						</div>
-					</div>
+      <section id="milestone" className="py-5">
+        <div className="container">
+          {contectLoop ? contectLoop : <BeatLoader />}
+        </div>
+      </section>
 
-					<div className='col-lg-3 text-center'>
-						<div className="space-content">
-							<Card className="value-content-bg rounded-4">
-								<div>
-									<RiTeamFill className="mx-4 mt-5 text-center" size={"50px"} color={"white"}/>
-								</div>
-								<Card.Body>
-									<Card.Title className="font-poppins text-white mt-2"><h4 className="fw-700">TEAMWORK</h4></Card.Title>
-									<Card.Text className="card-align text-white text-center pb-3 pt-2 ">
-									We recognize that our customers are the reason for our success in business, and we are committed to listen and respond positively to their needs.
-									</Card.Text>
-								</Card.Body>
-							</Card>
-						</div>
-					</div>
-					</div>
-					
-				</div>
-			</section>
+      {/* values section start */}
 
-				{/* values section end */}
-		</>
-	);
+      <section id="values" className="values">
+        <div className="position-relative">
+          <h1 className="text-center value-align position-absolute text-light">
+            {ValusData[0]?.module_name}
+          </h1>
+          <img
+            src={ValusData[0]?.module_image}
+            alt=""
+            className="values-bg-img"
+          />
+        </div>
+      </section>
+      <section id="values-content" className="">
+        <div className="container">
+          <div className="row">{valusCard ? valusCard : <BeatLoader />}</div>
+        </div>
+      </section>
+
+      {/* values section end */}
+    </>
+  );
 };
 
 export default PageMilestone;
--- pages/about/CompanyReview.js
+++ pages/about/CompanyReview.js
@@ -1,28 +1,30 @@
 import MileStoneContent from '../../components/Layouts/about/MilestoneContent'
-
 import { getcotentModule} from '../api/';
 import { useEffect,useState } from "react"
 
 
 const CompanyReview = () => {
+  const [companyData,setcompanyData] = useState([])
+  const [ValusData,setValusData] = useState([])
 
   useEffect(()=>{
     try {
       let moduleId = 7 // module id = AboutPage->CompanyReview
+      let ValusId = 20 // module id = AboutPage->CompanyReview
       getcotentModule(moduleId).then((Result)=>{
         setcompanyData(Result)
+      })
+      getcotentModule(ValusId).then((Result)=>{
+        setValusData(Result)
       })
     } catch (error) {
       console.log(error)
     }
   },[])
 
-  const [companyData,setcompanyData] = useState([])
-
-
   return (
     <>
-      <MileStoneContent sendcompanyData={companyData[0]}/>
+      <MileStoneContent ValusData={ValusData} sendcompanyData={companyData[0]}/>
     </>
   )
 }
